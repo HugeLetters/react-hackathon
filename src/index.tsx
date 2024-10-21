aeb305a62db542bf1d55ce3341748fd8c533f626
Evgenii Perminov <evperminov@ozon.ru>
2024-10-22 00:45:12 +0200

    setup biome

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <div>hello</div>
-  </StrictMode>
+const root = document.getElementById("root");
+
+if (!root) {
+	throw new Error("Could not find root element");
+}
+
+createRoot(root).render(
+	<StrictMode>
+		<div>hello</div>
+	</StrictMode>,
 );

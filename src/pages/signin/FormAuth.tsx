74216e9d1526ef51fb4c4f0f635ff11154fe18e8
Evgenii Perminov <evperminov@ozon.ru>
2024-10-26 23:03:00 +0200

    cleanup form auth

--- src/pages/signin/FormAuth.tsx
+++ src/pages/signin/FormAuth.tsx
@@ -2,12 +2,8 @@ import { Visibility, VisibilityOff } from "@mui/icons-material";
 import {
 	Button,
 	FormControl,
-	FormHelperText,
 	IconButton,
-	Input,
 	InputAdornment,
-	InputLabel,
-	OutlinedInput,
 	TextField,
 } from "@mui/material";
 import { useSignIn } from "@pages/signin/api";
@@ -20,19 +16,9 @@ export function FormAuth() {
 	const [errorPassword, setErrorPassword] = useState<string | null>(null);
 	const [showPassword, setShowPassword] = useState(false);
 
-	const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-	const handleMouseDownPassword = (
-		event: React.MouseEvent<HTMLButtonElement>,
-	) => {
-		event.preventDefault();
-	};
-
-	const handleMouseUpPassword = (
-		event: React.MouseEvent<HTMLButtonElement>,
-	) => {
-		event.preventDefault();
-	};
+	function handleClickShowPassword() {
+		setShowPassword((show) => !show);
+	}
 
 	return (
 		<Form
@@ -40,23 +26,20 @@ export function FormAuth() {
 			method="post"
 			onSubmit={(e) => {
 				e.preventDefault();
-				setErrorEmail(null);
-				setErrorPassword(null);
 
 				const data = new FormData(e.currentTarget);
-				const email = data.get("email")?.toString();
+				const email = data.get("email")?.toString().trim();
 				const password = data.get("password")?.toString().trim();
 
-				console.log("form", email, password);
+				const isValid = email ? /\S+@\S+/.test(email) : false;
+				setErrorEmail(isValid ? null : "Введите корректный email-адрес");
+				setErrorPassword(password ? null : "Введите корректный пароль");
 
-				const isValid = email ? /\S+@\S+\.\S+/.test(email) : null;
-				setErrorEmail(
-					!!email ? (isValid ? null : "not valid email") : "not valid email",
-				);
-				setErrorPassword(!!password ? null : "not valid password");
+				if (!isValid || !password) {
+					return;
+				}
 
-				(!!errorEmail || !!errorPassword) &&
-					signin({ body: { login: email, password } });
+				signin({ body: { login: email, password } });
 			}}
 			style={{ display: "flex", flexDirection: "column", maxWidth: "485px" }}
 		>
@@ -68,44 +51,43 @@ export function FormAuth() {
 					label="Логин"
 					placeholder="Введите e-mail"
 					sx={{ marginBottom: "30px", position: "relative" }}
+					error={!!errorEmail}
+					helperText={errorEmail}
 				/>
-				{errorEmail && (
-					<FormHelperText error sx={{ position: "absolute", top: "58px" }}>
-						Введите корректный email-адрес
-					</FormHelperText>
-				)}
 			</FormControl>
 
 			<FormControl variant="outlined">
-				<InputLabel htmlFor="outlined-adornment-password">Пароль</InputLabel>
-				<OutlinedInput
-					id="outlined-adornment-password"
+				<TextField
 					type={showPassword ? "text" : "password"}
 					placeholder="Введите пароль"
 					name="password"
 					sx={{ marginBottom: "40px", position: "relative" }}
-					endAdornment={
-						<InputAdornment position="end">
-							<IconButton
-								aria-label={
-									showPassword ? "hide the password" : "display the password"
-								}
-								onClick={handleClickShowPassword}
-								onMouseDown={handleMouseDownPassword}
-								onMouseUp={handleMouseUpPassword}
-								edge="end"
-							>
-								{showPassword ? <VisibilityOff /> : <Visibility />}
-							</IconButton>
-						</InputAdornment>
-					}
-					label="Password"
+					slotProps={{
+						input: {
+							endAdornment: (
+								<InputAdornment
+									position="end"
+									aria-label={
+										showPassword ? "hide the password" : "display the password"
+									}
+									onClick={handleClickShowPassword}
+									sx={{
+										background: "none",
+										outline: "1px solid transparent",
+										border: "1px solid transparent",
+									}}
+								>
+									<IconButton edge="end">
+										{showPassword ? <VisibilityOff /> : <Visibility />}
+									</IconButton>
+								</InputAdornment>
+							),
+						},
+					}}
+					label="Пароль"
+					error={!!errorPassword}
+					helperText={errorPassword}
 				/>
-				{errorPassword && (
-					<FormHelperText sx={{ position: "absolute", top: "58px" }} error>
-						Введите корректный пароль
-					</FormHelperText>
-				)}
 			</FormControl>
 
 			<Button variant="contained" size="large" type="submit">

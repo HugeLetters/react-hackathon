b82a88f9d9e7ca6893708554f8b1ec6f8c4dd43e
Evgenii Perminov <evperminov@ozon.ru>
2024-10-26 16:45:59 +0200

    fix header styles

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,20 +1,13 @@
 import logo from "@assets/icons/Logo.svg";
 import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import { HeaderBtn } from "./HeaderBtn";
-
-const toolbarStyle = {
-	width: "1500px",
-	margin: "0 auto",
-	height: "84px",
-	display: "flex",
-	justifyContent: "space-between",
-};
+import style from "./Header.module.css";
 
 export function Header() {
 	return (
 		<header>
 			<AppBar position="sticky" color="inherit">
-				<Toolbar style={toolbarStyle}>
+				<Toolbar className={style.toolbar}>
 					<Box component="img" src={logo} alt="Логотип" />
 					<Typography component="p">Запросы о помощи</Typography>
 					<HeaderBtn />
